4196757f8ac0e82e85e46c247786b837791b0aa9
saranya2808 <vemireddysaranyareddy@gmail.com>
2025-02-28 12:43:37 +0530

    saranya.1

--- add.cpp
+++ add.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     // Declare variables
-    int num1, num2, result;
+    int num1, num2, num3 result;
 
     // Initialize variables
     num1 = 100;
     num2 = 506;
 
     // Perform subtraction
-    result = num1 - num2;
+    result = num1 - num2 - num3;
 
     // Display the result
     printf("The result of %d - %d is %d\n", num1, num2, result);
